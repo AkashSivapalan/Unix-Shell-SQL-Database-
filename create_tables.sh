ebd908a34f21389b39d9a1c522269b04170900bc
AkashSivapalan <76577397+AkashSivapalan@users.noreply.github.com>
2021-01-03 22:23:52 -0500

    Update create_tables.sh

--- create_tables.sh
+++ create_tables.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
     export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
     
-    username="a7sivapa"
-    password="07257795"
+    username="username"
+    password="password"
     
     sqlplus64 "$username/$password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" < ./create_tables
